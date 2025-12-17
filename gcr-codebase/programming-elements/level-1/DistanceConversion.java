34b1f0580c5eb9344ee657572204f9f932ed37ec
Pranshi Patel <patelpranshi88@gmail.com>
2025-12-17 16:59:51 +0530

    [Pranshi Patel] : added Kilometers to Miles Conversion Program

---
+++ gcr-codebase/programming-elements/level-1/DistanceConversion.java
@@ -0,0 +1,19 @@
+// program to convert the distance of kilometers to miles
+
+public class DistanceConversion{
+	
+	public static void main (String[] args){
+		
+		// Take Distance in Kilometers
+		double kilometers = 10.8;
+		
+		// Convert the distance of kilometer to miles
+		double miles = kilometers * 1.6;
+		
+		// Print the distance of kilometer to miles
+		System.out.println("The Distance " + kilometers + "km in miles is : " + miles + " miles ");
+		
+	}
+	
+}
+			
