243dcc8b72495e41e2f684c37c86285f7d031253
Pranshi Patel <patelpranshi88@gmail.com>
2025-12-19 23:22:21 +0530

    [Pranshi Patel] : added Frequency of the number Program

---
+++ gcr-codebase/arrays/level-2/FrequencyNumber.java
@@ -0,0 +1,5 @@
+// Program to take a number as input find the frequency of each digit in the number using an array and display the frequency of each digit
+
+import java.util.Scanner;
+
+public class FrequencyNumber{
