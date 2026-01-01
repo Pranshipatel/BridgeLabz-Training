b13b43e5c6c846ff35a3e9b52b99b6f8dd2832fb
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-01 12:04:01 +0530

    [Pranshi Patel] : added Customer Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/objectmodelling/level1/CustomerProduct.java
@@ -0,0 +1,13 @@
+package com.objectmodelling.level1;
+
+public class CustomerProduct {
+    private String productName;
+
+    public CustomerProduct(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+}
