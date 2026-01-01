e921d799ebd7045d0d1afb83b63f4f15262737d7
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-01 12:09:21 +0530

    [Pranshi Patel] : added University Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/objectmodelling/level1/UniversityFaculty.java
@@ -0,0 +1,14 @@
+package com.objectmodelling.level1;
+
+public class UniversityFaculty {
+	 private String facultyName;
+
+	    public UniversityFaculty(String facultyName) {
+	        this.facultyName = facultyName;
+	    }
+
+	    public void displayFaculty() {
+	        System.out.println("Faculty Name: " + facultyName);
+	    }
+
+}
