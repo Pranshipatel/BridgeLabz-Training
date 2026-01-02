5991cb62a6edcbd944774bc78dd9b29b939131d6
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-02 15:03:20 +0530

    [Pranshi Patel] : added Vehicle Rental System Encapsulation and Polymorphishm Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/vehiclerentalsystem/Bike.java
@@ -0,0 +1,37 @@
+package com.encapsulationandpolymorphism.vehiclerentalsystem;
+
+
+
+public class Bike extends Vehicle {
+
+    private String insurancePolicyNumber;
+
+    public Bike(String vehicleNumber, double rentalRate, String policyNumber) {
+        super(vehicleNumber, "Bike", rentalRate);
+        this.insurancePolicyNumber = policyNumber;
+    }
+
+    public String getInsurancePolicyNumber() {
+        return insurancePolicyNumber;
+    }
+
+    public void setInsurancePolicyNumber(String insurancePolicyNumber) {
+        this.insurancePolicyNumber = insurancePolicyNumber;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return getRentalRate() * days;
+    }
+
+    @Override
+    public double calculateInsurance() {
+        return 200;
+    }
+
+    @Override
+    public String getInsuranceDetails() {
+        return "Bike Insurance Policy: ****" +
+                insurancePolicyNumber.substring(insurancePolicyNumber.length() - 4);
+    }
+}
