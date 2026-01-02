b1e00233d43576f84d642abf45a42b181213f1de
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-02 11:48:34 +0530

    [Pranshi Patel] : added Bank Account Hierarchical Inheritance Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/hierarchical/bankaccounttype/CheckingAccount.java
@@ -0,0 +1,15 @@
+package com.inheritance.hierarchical.bankaccounttype;
+
+class CheckingAccount extends BankAccount {
+    private int withdrawalLimit;
+
+    CheckingAccount(int accountNumber, double balance, int withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    void displayAccountType() {
+        System.out.println("Account Type: Checking Account");
+        System.out.println("Withdrawal Limit: " + withdrawalLimit + " per day");
+    }
+}
