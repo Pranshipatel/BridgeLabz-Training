97a544f6590f0865f4e49fa3c64b99c2e063adda
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-02 11:53:05 +0530

    [Pranshi Patel] : added Vehicle Management Hybrid Inheritance Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/hybrid/vehiclemanagement/Vehicle.java
@@ -0,0 +1,16 @@
+package com.inheritance.hybrid.vehiclemanagement;
+
+public class Vehicle {
+    String model;
+    int maxSpeed;
+
+    Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    void displayDetails() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
