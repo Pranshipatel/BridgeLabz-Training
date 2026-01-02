d0916b5eacfc042d7d399585cf5c38151e36177e
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-02 14:18:08 +0530

    [Pranshi Patel] : added ECommerce Platform Encapsulation and Polymorphishm Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/ecommerceplatform/Taxable.java
@@ -0,0 +1,10 @@
+package com.encapsulationandpolymorphism.ecommerceplatform;
+
+public interface Taxable {
+	// Method to calculate tax amount
+	double calculateTax();
+
+	// Method to return tax details
+	String getTaxDetails();
+
+}
