e99f387c255bff42e9eacd039747f046faae9708
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-02 13:04:23 +0530

    [Pranshi Patel] : added Employee Management Encapsulation and Polymorphishm Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/employeemanagement/Department.java
@@ -0,0 +1,7 @@
+package com.encapsulationandpolymorphism.employeemanagement;
+
+public interface Department {
+	public void assignDepartment(String department);
+	String getDepartmentDetails();
+
+}
