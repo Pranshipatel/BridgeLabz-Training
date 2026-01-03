c4c97e7cf2173484aa5a9205622485066da9e9e1
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-03 12:31:51 +0530

    [Pranshi Patel] : added Smart Automation System Program

---
+++ java-oops-practice/scenario-based/javaScenario/src/smarthomeautomationsystem/AC.java
@@ -0,0 +1,20 @@
+package smarthomeautomationsystem;
+
+class AC extends Appliance {
+
+    public AC() {
+        super(1500);
+    }
+
+    @Override
+    public void turnOn() {
+        setPowerStatus(true);
+        System.out.println("AC cooling started.");
+    }
+
+    @Override
+    public void turnOff() {
+        setPowerStatus(false);
+        System.out.println("AC turned OFF.");
+    }
+}
