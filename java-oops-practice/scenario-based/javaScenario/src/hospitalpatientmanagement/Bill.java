442035ae8faaf9035a5e1f657c7d36163c6cec82
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-03 12:27:36 +0530

    [Pranshi Patel] : added Hospital Patient Management Program

---
+++ java-oops-practice/scenario-based/javaScenario/src/hospitalpatientmanagement/Bill.java
@@ -0,0 +1,20 @@
+package hospitalpatientmanagement;
+
+class Bill implements Payable {
+    private double baseAmount;
+    private double taxRate;
+    private double discount;
+
+    public Bill(double baseAmount, double taxRate, double discount) {
+        this.baseAmount = baseAmount;
+        this.taxRate = taxRate;
+        this.discount = discount;
+    }
+
+    @Override
+    public double calculatePayment() {
+        double tax = baseAmount * taxRate;       // operators
+        double total = baseAmount + tax - discount;
+        return total;
+    }
+}
