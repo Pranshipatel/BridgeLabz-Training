efadc287c081571cd0ba2cd303b72e8789d02371
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-04 23:17:41 +0530

    [Pranshi Patel] : added Bank Account Encapsulation And Polymorphism Program

--- java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/bankingsystem/BankAccount.java
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/bankingsystem/BankAccount.java
@@ -1,6 +1,41 @@
 package com.encapsulationandpolymorphism.bankingsystem;
 
-public class BankAccount {
+public abstract class BankAccount {
 
+    private int accountNumber;
+    private String holderName;
+    private double balance;
 
+    public BankAccount(int accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    // Encapsulation
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
 }
