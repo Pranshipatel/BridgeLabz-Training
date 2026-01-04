a5f4cd3fd41166edf60d62365e48110bca0c60f6
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-04 23:18:20 +0530

    [Pranshi Patel] : added Bank Account Encapsulation And Polymorphism Program

--- java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/bankingsystem/CurrentAccount.java
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/bankingsystem/CurrentAccount.java
@@ -1,5 +1,23 @@
 package com.encapsulationandpolymorphism.bankingsystem;
 
-public class CurrentAccount {
+public class CurrentAccount extends BankAccount implements Loanable {
 
+    public CurrentAccount(int accNo, String name, double balance) {
+        super(accNo, name, balance);
+    }
+
+    @Override
+    public double calculateInterest() {
+        return getBalance() * 0.02; // 2% interest
+    }
+
+    @Override
+    public void applyForLoan(double amount) {
+        System.out.println("Business loan applied: ₹" + amount);
+    }
+
+    @Override
+    public boolean calculateLoanEligibility() {
+        return getBalance() >= 100000;
+    }
 }
