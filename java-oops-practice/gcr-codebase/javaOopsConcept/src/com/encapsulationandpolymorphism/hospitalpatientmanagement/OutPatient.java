8540823eb53153a71cb81066cdbefdda3334123b
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-05 00:15:29 +0530

    [Pranshi Patel] : added Hospital Patient Management Encapsulation and Polymorphism Program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/hospitalpatientmanagement/OutPatient.java
@@ -0,0 +1,16 @@
+package com.encapsulationandpolymorphism.hospitalpatientmanagement;
+
+public class OutPatient extends Patient {
+
+    private double consultationFee;
+
+    public OutPatient(int id, String name, int age, double consultationFee) {
+        super(id, name, age);
+        this.consultationFee = consultationFee;
+    }
+
+    @Override
+    public double calculateBill() {
+        return consultationFee;
+    }
+}
