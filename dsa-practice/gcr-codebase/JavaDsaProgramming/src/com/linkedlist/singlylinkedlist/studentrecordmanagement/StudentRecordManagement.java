397f8d583958c0f3582286f69effebe1c7eef413
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-05 10:46:51 +0530

    [Pranshi Patel] : added Student Record Management Program

---
+++ dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/linkedlist/singlylinkedlist/studentrecordmanagement/StudentRecordManagement.java
@@ -0,0 +1,31 @@
+package com.linkedlist.singlylinkedlist.studentrecordmanagement;
+
+//Main class to test Student Linked List
+public class StudentRecordManagement {
+
+public static void main(String[] args) {
+
+   StudentLinkedList list = new StudentLinkedList();
+
+   // Adding students
+   list.addAtBeginning(1, "Arif", 21, 'A');
+   list.addAtEnd(2, "Rahul", 22, 'B');
+   list.addAtEnd(3, "Sneha", 20, 'A');
+   list.addAtPosition(2, 4, "Neha", 21, 'C');
+
+   // Display records
+   list.displayAll();
+
+   // Search student
+   list.searchByRollNumber(2);
+
+   // Update grade
+   list.updateGrade(3, 'A');
+
+   // Delete student
+   list.deleteByRollNumber(1);
+
+   // Display updated list
+   list.displayAll();
+}
+}
