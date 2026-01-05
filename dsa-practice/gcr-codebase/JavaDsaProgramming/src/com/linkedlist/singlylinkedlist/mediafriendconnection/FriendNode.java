45f38740e2ac761197c6dda693df76b0398d22ad
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-05 10:58:02 +0530

    [Pranshi Patel] : added Media Friend Connection Program

---
+++ dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/linkedlist/singlylinkedlist/mediafriendconnection/FriendNode.java
@@ -0,0 +1,13 @@
+package com.linkedlist.singlylinkedlist.mediafriendconnection;
+
+//Node to store Friend IDs
+public class FriendNode {
+
+int friendId;
+FriendNode next;
+
+public FriendNode(int friendId) {
+   this.friendId = friendId;
+   this.next = null;
+}
+}
