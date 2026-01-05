7f21c97fa90222b78afa0c368266287c3b90a571
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-05 10:37:07 +0530

    [Pranshi Patel] : added Round Robin Scheduling Algorithm

---
+++ dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/linkedlist/circularlinkedlist/roundrobin/RoundRobinApp.java
@@ -0,0 +1,17 @@
+package com.linkedlist.circularlinkedlist.roundrobin;
+
+//Main class to test Round Robin Scheduling
+public class RoundRobinApp {
+
+public static void main(String[] args) {
+
+   RoundRobinScheduler scheduler = new RoundRobinScheduler(4); // Time Quantum = 4
+
+   scheduler.addProcess(1, 10, 1);
+   scheduler.addProcess(2, 5, 2);
+   scheduler.addProcess(3, 8, 1);
+   scheduler.addProcess(4, 6, 3);
+
+   scheduler.schedule();
+}
+}
