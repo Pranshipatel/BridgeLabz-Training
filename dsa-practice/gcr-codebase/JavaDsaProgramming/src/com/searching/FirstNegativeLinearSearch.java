06c6f762d13507298aa3ea758f077899eca2ea74
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-07 00:26:49 +0530

    [Pranshi Patel] : added First Negative Linear Search

---
+++ dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/searching/FirstNegativeLinearSearch.java
@@ -0,0 +1,29 @@
+package com.searching;
+
+public class FirstNegativeLinearSearch {
+
+    public static int findFirstNegative(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] < 0) {
+                return i;   // First negative found
+            }
+        }
+
+        return -1;  // No negative number found
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 8, 3, -4, 7, -2};
+
+        int index = findFirstNegative(numbers);
+
+        if (index != -1) {
+            System.out.println("First negative number found at index: " + index);
+        } else {
+            System.out.println("No negative number found in the array");
+        }
+    }
+}
