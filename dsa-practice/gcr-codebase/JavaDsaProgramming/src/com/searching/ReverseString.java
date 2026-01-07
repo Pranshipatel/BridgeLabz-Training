a2e56a8191160136008204767e5d78025b0af956
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-07 11:36:06 +0530

    [Pranshi Patel] : added Reverse String Program

---
+++ dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/searching/ReverseString.java
@@ -0,0 +1,12 @@
+package com.searching;
+import java.util.*;
+public class ReverseString {
+	public static void main(String[] args) {
+		String str = "Pranshi";
+		StringBuilder sb = new StringBuilder(str);
+		sb.reverse();
+		System.out.println(sb.toString());
+		
+	}
+
+}
