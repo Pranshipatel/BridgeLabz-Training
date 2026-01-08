8b55a579bf58b4dc4c6c6cb1a3722a5fe9887bbe
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-08 11:14:01 +0530

    [Pranshi Patel] : added Book Bazar Program

---
+++ java-oops-practice/scenario-based/javaScenario/src/LoanBuddy/Application.java
@@ -0,0 +1,38 @@
+package LoanBuddy;
+
+public class Application {
+	private String name;
+	private int creditScore;
+	private int income;     
+	private int loadAmount;
+	
+	
+	public Application(String name, int creditScore, int income, int loadAmount) {
+
+		this.name = name;
+		this.creditScore = creditScore;
+		this.income = income;
+		this.loadAmount = loadAmount;
+	}
+	
+	public String getName() {
+        return name;
+    }
+
+	
+	protected int getCreditScore() {
+		return creditScore;
+	}
+
+	protected int getIncome() {
+		return income;
+	}
+
+	protected int getLoadAmount() {
+		return loadAmount;
+	}
+
+	
+	
+
+}
