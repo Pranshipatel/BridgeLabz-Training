13a74dadb3104589b1272234a5a03297b837d56b
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-10 11:37:08 +0530

    [Pranshi Patel] : added Event Ease App Program

--- java-oops-practice/scenario-based/javaScenario/src/eventease/ConferenceEvent.java
+++ java-oops-practice/scenario-based/javaScenario/src/eventease/ConferenceEvent.java
@@ -1,5 +1,27 @@
 package eventease;
 
-public class ConferenceEvent {
+public class ConferenceEvent extends Event {
 
+    public ConferenceEvent(int eventId, String location, String date,
+                            int attendees, double venueCost, User organizer) {
+
+        super(eventId, "Conference", location, date,
+              attendees, venueCost, organizer);
+    }
+
+    public ConferenceEvent(int eventId, String location, String date,
+                            int attendees, double venueCost,
+                            double serviceCost, double discount, User organizer) {
+
+        super(eventId, "Conference", location, date,
+              attendees, venueCost, serviceCost, discount, organizer);
+    }
+
+    @Override
+    public void schedule() {
+        System.out.println("📊 Conference Event Scheduled!");
+        System.out.println("Organizer: " + organizer.getName());
+        System.out.println("Attendees: " + attendees);
+        System.out.println("Total Cost: ₹" + calculateTotalCost());
+    }
 }
