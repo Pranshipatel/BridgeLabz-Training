5e159514b4a57645cd104e3050633f235651fa53
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-10 11:35:46 +0530

    [Pranshi Patel] : added Event Ease App Program

--- java-oops-practice/scenario-based/javaScenario/src/eventease/BirthdayEvent.java
+++ java-oops-practice/scenario-based/javaScenario/src/eventease/BirthdayEvent.java
@@ -1,5 +1,26 @@
 package eventease;
 
-public class BirthdayEvent {
+public class BirthdayEvent extends Event {
 
+    public BirthdayEvent(int eventId, String location, String date,
+                          int attendees, double venueCost, User organizer) {
+
+        super(eventId, "Birthday Party", location, date,
+              attendees, venueCost, organizer);
+    }
+
+    public BirthdayEvent(int eventId, String location, String date,
+                          int attendees, double venueCost,
+                          double serviceCost, double discount, User organizer) {
+
+        super(eventId, "Birthday Party", location, date,
+              attendees, venueCost, serviceCost, discount, organizer);
+    }
+
+    @Override
+    public void schedule() {
+        System.out.println("🎉 Birthday Event Scheduled!");
+        System.out.println("Organizer: " + organizer.getName());
+        System.out.println("Total Cost: ₹" + calculateTotalCost());
+    }
 }
