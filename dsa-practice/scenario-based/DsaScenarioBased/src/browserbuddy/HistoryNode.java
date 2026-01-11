5acfe91865121932f70675332520c5aea08e3156
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-11 22:41:39 +0530

    [Pranshi Patel] : added Browser Buddy Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/browserbuddy/HistoryNode.java
@@ -0,0 +1,12 @@
+package browserbuddy;
+
+class HistoryNode {
+    String url;
+    HistoryNode prev;
+    HistoryNode next;
+
+    HistoryNode(String url) {
+        this.url = url;
+    }
+    
+}
