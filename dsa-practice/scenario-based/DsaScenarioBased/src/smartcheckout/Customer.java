008467523ee3713d797ef042f37aefd2aeaae360
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-13 12:37:53 +0530

    [Pranshi Patel] : added Smart Checkout Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/smartcheckout/Customer.java
@@ -0,0 +1,14 @@
+package smartcheckout;
+
+import java.util.List;
+
+public class Customer {
+	String name;
+	List<String> item;
+	
+	public Customer(String name, List<String> item) {
+		this.name = name;
+		this.item = item;
+	}
+
+}
