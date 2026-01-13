538d3c06b8e3ad1edb9deb79a309ce9f1229a493
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-13 12:36:15 +0530

    [Pranshi Patel] : added Call Center Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/callcenter/Customer.java
@@ -0,0 +1,18 @@
+package callcenter;
+
+public class Customer {
+	
+	String customerId;
+	String name;
+	boolean isVIP;
+	
+	
+	public Customer(String customerId, String name, boolean isVIP) {
+		super();
+		this.customerId = customerId;
+		this.name = name;
+		this.isVIP = isVIP;
+	}
+	
+
+}
