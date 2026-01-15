c8f3c259af654ed015ec259bb876e77a5b5cbccc
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-16 00:40:34 +0530

    [Pranshi Patel] : added Smart Shelf Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/smartshelf/Book.java
@@ -0,0 +1,15 @@
+package smartshelf;
+
+public class Book {
+	String title;
+	String author;
+	String genre;
+	
+	
+	public Book(String title, String author, String genre) {
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+	}
+
+}
