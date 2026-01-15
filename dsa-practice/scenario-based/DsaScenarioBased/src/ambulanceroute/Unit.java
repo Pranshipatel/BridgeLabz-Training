4618e005f3560b1d0df3504bd2dc4e54c05eaecb
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-16 00:42:23 +0530

    [Pranshi Patel] : added Ambulance Route Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/ambulanceroute/Unit.java
@@ -0,0 +1,20 @@
+package ambulanceroute;
+
+//Represents a hospital unit in the circular linked list
+class Unit {
+
+	// Name of the hospital unit
+	String name;
+
+	// Availability status of the unit
+	boolean available;
+
+	// Pointer to the next unit (circular link)
+	Unit next;
+
+	// Constructor to initialize unit details
+	Unit(String name, boolean available) {
+		this.name = name;
+		this.available = available;
+	}
+}
