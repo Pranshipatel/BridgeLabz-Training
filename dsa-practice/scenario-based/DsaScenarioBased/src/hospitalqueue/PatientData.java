62af23e2c1f698a8fe2d6cd82381240e9349310e
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-16 00:41:49 +0530

    [Pranshi Patel] : added Hospital Queue Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/hospitalqueue/PatientData.java
@@ -0,0 +1,15 @@
+package hospitalqueue;
+
+public class PatientData {
+	String name;
+	int age;
+	int criticalLevel;
+	
+	public PatientData(String name, int age, int criticalLevel) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.criticalLevel = criticalLevel;
+	}
+
+}
