91770dc5404727c552476a27a1991cbf04b78691
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-17 15:34:49 +0530

    [Pranshi Patel] : added Ice Cream Rush Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/icecreamrush/IceCream.java
@@ -0,0 +1,16 @@
+package icecreamrush;
+
+/**
+ * 
+ */
+public class IceCream {
+	String flavour;
+	int sellCount;
+	
+	public IceCream(String flavour, int sellCount) {
+		this.flavour = flavour;
+		this.sellCount = sellCount;
+	}
+	
+
+}
