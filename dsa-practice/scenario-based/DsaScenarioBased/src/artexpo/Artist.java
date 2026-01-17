97bc99f39d7bd8c637ea212b293a3dea33670c42
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-17 15:32:11 +0530

    [Pranshi Patel] : added ArtExpo Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/artexpo/Artist.java
@@ -0,0 +1,14 @@
+package artexpo;
+import java.time.LocalTime;
+
+public class Artist {
+	String name;
+	LocalTime currentTime;
+	public Artist(String name, LocalTime currentTime) {
+		super();
+		this.name = name;
+		this.currentTime = currentTime;
+	}
+	
+
+}
