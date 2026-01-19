f6f14612b0875a38cf49b7d8a5cf44d40b3dd169
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-19 16:37:31 +0530

    [Pranshi Patel] : added Exam Cell Program

---
+++ dsa-practice/scenario-based/DsaScenarioBased/src/examcell/Student.java
@@ -0,0 +1,16 @@
+package examcell;
+
+public class Student {
+	String name;
+	int score;
+	String testCenter;
+
+	// Parameterized Constructor
+	public Student(String name, int score, String testCenter) {
+
+		this.name = name;
+		this.score = score;
+		this.testCenter = testCenter;
+	}
+
+}
