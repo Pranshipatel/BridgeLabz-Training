d97415bd34d119ee2299d2e264b13108d40d8bb6
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-20 15:56:26 +0530

    [Pranshi Patel] : added Online MarketPlace Program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/generics/onlinemarketplace/ClothingCategory.java
@@ -0,0 +1,10 @@
+package com.generics.onlinemarketplace;
+
+//Represents clothing-related products
+class ClothingCategory implements Category {
+
+	// Returns category name
+	public String getCategoryName() {
+		return "Clothing";
+	}
+}
