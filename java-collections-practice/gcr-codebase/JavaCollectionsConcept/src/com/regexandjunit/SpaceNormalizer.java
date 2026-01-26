c963f723a608dabb5b52c9b83d56e5b371184ab1
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-26 23:17:05 +0530

    [Pranshi Patel] : added Space Normalizer Program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/regexandjunit/SpaceNormalizer.java
@@ -0,0 +1,15 @@
+package com.regexandjunit;
+
+public class SpaceNormalizer {
+
+	public static void main(String[] args) {
+
+		String input = "This   is   an    example   with    multiple   spaces.";
+
+		// Replace multiple spaces with a single space
+		String output = input.replaceAll("\\s+", " ");
+
+		// Print result
+		System.out.println(output);
+	}
+}
