49c05c806676e76ebb35d3b48d2bcfc8e6da292a
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-27 11:58:42 +0530

    [Pranshi Patel] : added private field Program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/reflectionandannotation/reflection/privatefield/Person.java
@@ -0,0 +1,12 @@
+package com.reflectionandannotation.reflection.privatefield;
+
+class Person {
+
+	// Private field
+	private int age = 20;
+
+	// Getter (optional, not used by reflection)
+	public int getAge() {
+		return age;
+	}
+}
