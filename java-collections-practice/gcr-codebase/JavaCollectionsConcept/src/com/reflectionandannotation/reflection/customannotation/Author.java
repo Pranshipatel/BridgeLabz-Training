799b6e88b658407779342cf6a05486456775adda
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-27 12:02:49 +0530

    [Pranshi Patel] : added Custom Annotation Program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcept/src/com/reflectionandannotation/reflection/customannotation/Author.java
@@ -0,0 +1,10 @@
+package com.reflectionandannotation.reflection.customannotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+// Annotation available at runtime
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Author {
+    String name();
+}
