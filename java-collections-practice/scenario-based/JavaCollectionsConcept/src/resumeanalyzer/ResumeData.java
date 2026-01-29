51dd7f258f810ca427fcf85f832e4a39f0399c0f
Pranshi Patel <patelpranshi88@gmail.com>
2026-01-29 11:58:28 +0530

    [Pranshi Patel] : added Resume Analyzer

---
+++ java-collections-practice/scenario-based/JavaCollectionsConcept/src/resumeanalyzer/ResumeData.java
@@ -0,0 +1,24 @@
+package resumeanalyzer;
+
+
+
+//Model class to store resume details
+public class ResumeData {
+
+	String email;
+	String phone;
+	int keywordCount;
+	String fileName;
+
+	public ResumeData(String email, String phone, int keywordCount, String fileName) {
+		this.email = email;
+		this.phone = phone;
+		this.keywordCount = keywordCount;
+		this.fileName = fileName;
+	}
+
+	@Override
+	public String toString() {
+		return fileName + " | Email: " + email + " | Phone: " + phone + " | Skill Matches: " + keywordCount;
+	}
+}
---
+++ java-collections-practice/scenario-based/JavaCollectionsConcept/src/resumeanalyzer/ResumeReader.java
@@ -0,0 +1,30 @@
+package resumeanalyzer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+// Reads resume content using I/O streams
+public class ResumeReader {
+
+	public static String readResume(File file) throws IOException {
+
+		// Skip unsupported formats
+		if (!file.getName().endsWith(".txt") && !file.getName().endsWith(".pdf")) {
+			throw new IOException("Invalid resume format");
+		}
+
+		// Simulate PDF as text (real system uses PDFBox)
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		StringBuilder content = new StringBuilder();
+		String line;
+
+		while ((line = br.readLine()) != null) {
+			content.append(line).append(" ");
+		}
+
+		br.close();
+		return content.toString();
+	}
+}
