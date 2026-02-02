d921c9488a35c75b9dbf29cd428583d048954d2d
Pranshi Patel <patelpranshi88@gmail.com>
2026-02-02 13:19:11 +0530

    [Pranshi Patel] : added Transforming Names Program

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/streamapi/TransformingNames.java
@@ -0,0 +1,32 @@
+package com.streamapi;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TransformingNames {
+	public static void main(String[] args) {
+		  List<String> customers = Arrays.asList(
+	                "Pranshi",
+	                "Riya",
+	                "Amit",
+	                "Rahul",
+	                "Neha",
+	                "Kunal",
+	                "Sneha",
+	                "Vikram",
+	                "Anjali",
+	                "Arjun"
+	        );
+		  
+		  List<String> sorted = customers.stream()
+				  .map(String :: toUpperCase)
+				  .sorted()
+				  .collect(Collectors.toList());
+		  
+		  sorted.forEach(System.out::println);
+
+	}
+
+}
