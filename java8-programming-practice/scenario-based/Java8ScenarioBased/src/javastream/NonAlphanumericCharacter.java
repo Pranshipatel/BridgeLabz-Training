4983af86dbca469b485bcda04cd95b13e84d89b4
Pranshi Patel <patelpranshi88@gmail.com>
2026-02-04 11:51:08 +0530

    [Pranshi Patel] : added Non Alphanumeric Character Program

---
+++ java8-programming-practice/scenario-based/Java8ScenarioBased/src/javastream/NonAlphanumericCharacter.java
@@ -0,0 +1,22 @@
+package javastream;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class NonAlphanumericCharacter {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the sentence");
+		String str = input.nextLine();
+		
+		List<String> output = str.chars()
+				.filter(e -> Character.isLetterOrDigit(e))
+				.mapToObj(e -> String.valueOf((char)e))
+				.collect(Collectors.toList());
+		
+		output.forEach(System.out :: print);
+		
+	}
+
+}
