4b395844dfe2791ae2d4b1451e669991245de48f
Pranshi Patel <patelpranshi88@gmail.com>
2026-02-04 11:55:50 +0530

    [Pranshi Patel] : added Sum Of Numbers Program

---
+++ java8-programming-practice/scenario-based/Java8ScenarioBased/src/logicalquestions/SumOfNumbers.java
@@ -0,0 +1,16 @@
+package logicalquestions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SumOfNumbers {
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		
+		int sum = list.stream().mapToInt(Integer :: intValue).sum();
+		
+		System.out.print("Sum of numbers : " + sum);
+	}
+
+}
